2a27d053671bfadb22f108d147be9e3b0c6c242f
Daniel Oñate <danionatee@gmail.com>
2020-11-16 23:18:56 -0500

    Incluido fecha

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from sodapy import Socrata
+from dateutil import parser
 from flask import Flask, render_template, request, redirect, url_for
 
 cliente = Socrata("www.datos.gov.co", None)
 results = cliente.get("gt2j-8ykr")
 
 cdf = pd.DataFrame.from_records(results).fillna(value='No Registra')
+cdf['fecha_de_notificaci_n'] = cdf['fecha_de_notificaci_n'].apply(parser.parse)
 
 app = Flask(__name__)
 
@@ -16,8 +18,9 @@ def index():
 @app.route('/filtrar', methods=['POST'])
 def filtrar():
     if request.method == 'POST':
-        if request.form['ciudad'] != '':
-            return render_template('index.html', datos=cdf[cdf['ciudad_municipio_nom'] == request.form['ciudad']].to_html())
+        if request.form['ciudad'] != '' and request.form['fecha'] != '':
+            return render_template('index.html', datos=cdf[(cdf['ciudad_municipio_nom'] == request.form['ciudad']) & (cdf['fecha_de_notificaci_n'] == request.form['fecha'])].to_html())
+
         else:
             return redirect(url_for('index'))
 
