dd0a0e35e8b50ce41887017b984ab1c7cc8f8912
Daniel Oñate <danionatee@gmail.com>
2020-11-16 23:38:46 -0500

    Incluido fecha

--- main.py
+++ main.py
@@ -18,8 +18,12 @@ def index():
 @app.route('/filtrar', methods=['POST'])
 def filtrar():
     if request.method == 'POST':
+
         if request.form['ciudad'] != '' and request.form['fecha'] != '':
             return render_template('index.html', datos=cdf[(cdf['ciudad_municipio_nom'] == request.form['ciudad']) & (cdf['fecha_de_notificaci_n'] == request.form['fecha'])].to_html())
+
+        elif request.form['ciudad'] != '' and request.form['fecha'] == '':
+            return render_template('index.html', datos=cdf[cdf['ciudad_municipio_nom'] == request.form['ciudad']].to_html())
 
         else:
             return redirect(url_for('index'))
